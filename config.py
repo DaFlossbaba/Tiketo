aacb17cffbda69fbfab097873ffa39c20605730f
DaFlossbaba <144553638+DaFlossbaba@users.noreply.github.com>
2025-07-24 15:49:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 SENDER_EMAIL = "email@gmail.com"                # Your gmail
 EMAIL_PASSWORD = "APPpassword"                  # App-passw0rd from gmail
-RECIPIENT_EMAIL = "oliver_bota@outlook.com"     # The recipient
+RECIPIENT_EMAIL = "recipient@mail"              # The recipient
 SMTP_SERVER = "smtp.gmail.com"                  # SMTP-server
 SMTP_PORT = 587                                 # encryption port
 
